813e508b01f21c8dba516805b80592c67dc78e28
junseongday <junseongday@gmail.com>
2020-03-06 16:21:34 +0900

    Initial commit

---
+++ src/main/java/com/js/demospringbootmonitor/Application.java
@@ -0,0 +1,16 @@
+package com.js.demospringbootmonitor;
+
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
